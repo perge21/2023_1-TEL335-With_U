da7c43a1ca8e9ee7a9e0ac5ae32f16998034210d
perge21 <perge21@DESKTOP-5AG0FHF>
2023-06-11 23:58:44 -0400

    Tercera Entrega: Implementación del 10% de frontend con operaciones CRUD en mongodb

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const mongoose = require("mongoose");
 const bodyParser = require("koa-bodyparser");
+const static = require('koa-static');
+const render = require('koa-ejs');
+const session = require('koa-session');
+const path = require('path');
+
 
 // Crea una nueva aplicación de Koa
 const app = new Koa();
 const router = new Router();
+
+// Configurar Koa-EJS
+render(app, {
+  root: path.join(__dirname, 'public'),
+  layout: false,
+  viewExt: 'ejs',
+  cache: false,
+  debug: false,
+});
+
+// Define la ruta para servir archivos estáticos
+const staticPath = path.join(__dirname, 'public');
+
+// Configuración de la sesión
+app.keys = ['clave-secreta']; // Clave secreta para firmar las cookies de sesión
+app.use(session(app));
 
 // Conéctate a la base de datos de MongoDB
 mongoose
@@ -27,30 +48,37 @@ const userSchema = new mongoose.Schema({
 });
 
 // Crea el modelo de usuarios
-const User = mongoose.model("User", userSchema);
 app.use(bodyParser());
 
+// Página de inicio personalizada
+router.get('/home', async (ctx) => {
+  const userName = ctx.session.userName || 'anónimo';
+  const notificacion = ctx.session.notificacion || 'indefinido';
+
+  await ctx.render('home', { name: userName, notificacion: notificacion });
+});
+
 // Agrega un usuario
-router.post("/usuarios", async (ctx, next) => {
+router.post("/a_registro", async (ctx, next) => {
   // Extrae los datos del cuerpo de la solicitud
   const { nombre, edad, notificacion } = ctx.request.body;
 
   // Crea un nuevo usuario con los datos proporcionados
-
   try {
-    // Guarda el usuario en la base de datos
+    // Guarda el usuario en la base de datos mediante insert
     const result = users_c.insertOne({
       nombre: nombre,
       edad: edad,
       notificacion: notificacion,
     });
     ctx.status = 201;
-    ctx.body = `Usuario insertado con éxito con el id ${result.insertedId}`;
+    ctx.redirect('/login');
   } catch (err) {
     ctx.response.status = 500;
     ctx.response.body = { error: err.message };
   }
 });
+
 
 router.get("/users", async (ctx) => {
   try {
@@ -62,15 +90,50 @@ router.get("/users", async (ctx) => {
   }
 });
 
+// Ruta para mostrar el formulario de inicio de sesión
+router.get('/login', async (ctx) => {
+  await ctx.render('login', { invalidUser: false });
+});
+
+router.post('/login', async (ctx) => {
+  const { username } = ctx.request.body;
+
+  try {
+    const user = await users_c.findOne({ nombre: username });
+
+    if (user) {
+      ctx.session.userName = username;
+      ctx.session.notificacion = user.notificacion;
+      ctx.redirect('/home'); // Redireccionar al home después de iniciar sesión
+    } else {
+      await ctx.render('login', { invalidUser: true });
+    }
+  } catch (error) {
+    console.error(error);
+    ctx.redirect('/login'); // Redireccionar de vuelta al formulario de inicio de sesión
+  }
+});
+
+
+router.get('/logout', async (ctx) => {
+  // Limpiar la sesión actual
+  ctx.session = null;
+
+  // Redirigir a la página de inicio de sesión u otra página
+  ctx.redirect('/home');
+});
+
+
 router.get("/", async (ctx) => {
   try {
-    ctx.body = "¡Hola, Mundo!";
+    
+
   } catch (err) {
     ctx.status = 500;
     ctx.body = { error: err.message };
   }
 });
-
+app.use(static(staticPath));
 // Agrega las rutas al enrutador de Koa
 app.use(router.routes()).use(router.allowedMethods());
 
